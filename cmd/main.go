2d09fbe0239564825b729ae7698cd114435e5f59
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2021-07-25 00:08:37 -0400

    Wait for server to become reachable

--- cmd/main.go
+++ cmd/main.go
@@ -6,23 +6,32 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/digitalocean/godo"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 
 	"github.com/alvaroaleman/vpnctrl/pkg/ini"
 	"github.com/alvaroaleman/vpnctrl/types"
 )
 
+const listenAddr = 51820
+
 type options struct {
 	interfaceName string
 }
 
 func main() {
-	log, err := zap.NewProduction()
+	logCfg := zap.NewProductionConfig()
+	logCfg.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	log, err := logCfg.Build()
 	if err != nil {
 		fmt.Printf("failed to construct logger: %v\n", err)
 		os.Exit(1)
@@ -33,21 +42,33 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to get hostname", zap.Error(err))
 	}
+	if uid := os.Geteuid(); uid != 0 {
+		log.Fatal("Must run as root, but ran with other UID", zap.Int("uid", uid))
+	}
 
-	doClient := godo.NewFromToken(os.Getenv("DO_TOKEN"))
+	doToken := os.Getenv("DO_TOKEN")
+	if doToken == "" {
+		log.Fatal("DO_TOKEN env var must be set and contain a valid DigitalOcean token")
+	}
+	doClient := godo.NewFromToken(doToken)
 
 	localCfg, serverPrivKey, err := generateLocalConfig()
 	if err != nil {
 		log.Fatal("failed to generate wireguard config", zap.Error(err))
 	}
 
-	serverAddr, err := createServer(context.Background(), doClient, serverPrivKey, localCfg.Interface.PrivateKey.PublicKey(), hostname)
+	serverAddr, err := createServer(context.Background(), log, doClient, serverPrivKey, localCfg.Interface.PrivateKey.PublicKey(), hostname)
 	if err != nil {
 		log.Fatal("failed to create server", zap.Error(err))
 	}
-	log.Info("Created server", zap.String("address", serverAddr))
+	log = log.With(zap.String("server_addr", serverAddr))
+	log.Info("Created server and got its address")
 
-	localCfg.Peer.Endpoint = serverAddr + ":51820"
+	if err := waitForServer(log, serverAddr); err != nil {
+		log.Fatal("Failed to wait for server to become reachable", zap.Error(err))
+	}
+
+	localCfg.Peer.Endpoint = serverAddr + ":" + strconv.Itoa(listenAddr)
 	localCfgSerialized, err := ini.Marshal(localCfg)
 	if err != nil {
 		log.Fatal("failed to marshal local wireguard config", zap.Error(err))
@@ -81,12 +102,18 @@ func generateLocalConfig() (cfg *types.WireguardConfig, serverPriv *wgtypes.Key,
 	}, &serverPrivKey, nil
 }
 
-func createServer(ctx context.Context, client *godo.Client, privKey *wgtypes.Key, clientKey wgtypes.Key, name string) (address string, err error) {
+func createServer(ctx context.Context,
+	log *zap.Logger,
+	client *godo.Client,
+	privKey *wgtypes.Key,
+	clientKey wgtypes.Key,
+	name string,
+) (address string, err error) {
 	wgCfg := types.WireguardConfig{
 		Interface: types.WireguardInterface{
 			Address:    "172.18.6.1/24",
 			PrivateKey: *privKey,
-			ListenPort: 51820,
+			ListenPort: listenAddr,
 			PostUp:     "echo 1 > /proc/sys/net/ipv4/ip_forward; iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE",
 			PostDown:   "iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE",
 			Table:      "off",
@@ -132,6 +159,7 @@ runcmd:
 	if err != nil {
 		return "", fmt.Errorf("failed to create server: %w", err)
 	}
+	log.Info("Successfully created server, waiting for IP to be assigned")
 
 	timeAfterChan := time.After(30 * time.Second)
 	id := server.ID
@@ -160,4 +188,41 @@ func publicAddr(server *godo.Droplet) string {
 	}
 
 	return ""
+}
+
+func waitForServer(log *zap.Logger, addr string) error {
+	cfg := &ssh.ClientConfig{
+		User:            "root",
+		Timeout:         10 * time.Second,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		cfg.Auth = append(cfg.Auth, ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers))
+	}
+
+	log.Info("Waiting for wireguard server to become reachable")
+	timeout := time.After(5 * time.Minute)
+	for {
+		select {
+		case <-timeout:
+			return fmt.Errorf("timeout waiting for %s to become reachable", addr)
+		default:
+			connection, err := ssh.Dial("tcp", addr+":22", cfg)
+			if err != nil {
+				log.Info("Error establishing ssh connection, retrying...", zap.Error(err))
+				continue
+			}
+			session, err := connection.NewSession()
+			if err != nil {
+				return fmt.Errorf("failed to open ssh session: %w", err)
+			}
+			if err := session.Run("wg show wg0"); err != nil {
+				log.Info("Wireguard server not yet up, retrying...")
+				continue
+			}
+
+			log.Info("Wireguard server became reachable")
+			return nil
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/digitalocean/godo v1.64.2
 	github.com/smartystreets/goconvey v1.6.4 // indirect
 	go.uber.org/zap v1.18.1
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
 	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20210506160403-92e472f520a5
 	gopkg.in/ini.v1 v1.62.0
 )
